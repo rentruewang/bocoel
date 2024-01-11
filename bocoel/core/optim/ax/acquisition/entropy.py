8fe587dbfa6a7ce534a0c808d4bbccc7576b2d0a
RenChu Wang <patrick1031wang@gmail.com>
2024-01-11 14:06:53 -0500

    🥴 Removed more unused variables.
    Even faster.
    
    Co-authored-by: Chien Yi Yang <chy036@ucsd.edu>

--- bocoel/core/optim/ax/acquisition/entropy.py
+++ bocoel/core/optim/ax/acquisition/entropy.py
@@ -1,5 +1,4 @@
 from botorch.acquisition import input_constructors, qMaxValueEntropy
-from botorch.models import utils
 from torch import Tensor
 
 
@@ -7,16 +6,15 @@ class Entropy(qMaxValueEntropy):
     def _compute_information_gain(
         self, X: Tensor, mean_M: Tensor, variance_M: Tensor, covar_mM: Tensor
     ) -> Tensor:
+        # Unused variables.
+        del mean_M, variance_M, covar_mM
+
         # compute the std_m, variance_m with noisy observation
         posterior_m = self.model.posterior(
             X.unsqueeze(-3),
             observation_noise=True,
             posterior_transform=self.posterior_transform,
         )
-
-        # batch_shape x num_fantasies x (m) x (1 + num_trace_observations)
-        variance_m = posterior_m.distribution.covariance_matrix
-        utils.check_no_nans(variance_m)
 
         # H0 = H(ym | x, Dt)
         H0 = posterior_m.distribution.entropy()  # batch_shape x num_fantasies x (m)
