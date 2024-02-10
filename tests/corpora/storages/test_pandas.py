b4beb5961a7c584f845e761adfb3e0dbc57c7846
RenChu Wang <patrick1031wang@gmail.com>
2024-02-10 08:08:00 -0500

    🧪 Fixed test.
    Forgot to update the fields.

--- tests/corpora/storages/test_pandas.py
+++ tests/corpora/storages/test_pandas.py
@@ -4,5 +4,5 @@ from . import factories
 def test_dataframe_storage() -> None:
     df = factories.df()
     dfs = factories.df_storage()
-    assert set(dfs.keys()) == {"question", "answer"}
+    assert set(dfs.keys()) == {"question", "answer", "answer_id"}
     assert dfs[0] == df.iloc[0].to_dict()
