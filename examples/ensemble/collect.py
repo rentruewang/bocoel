9fdc21900e03b0b62325c9df41648aeaa21b425e
RenChu Wang <patrick1031wang@gmail.com>
2024-02-12 17:33:54 -0500

    💰 Collector updated and simplified.
    This shall speed up the code.

--- examples/ensemble/collect.py
+++ examples/ensemble/collect.py
@@ -2,196 +2,225 @@ import itertools
 
 import fire
 import numpy as np
+import pandas as pd
 import parse
 import seaborn as sns
+import structlog
 from matplotlib import pyplot as plt
 from numpy.typing import NDArray
-from pandas import DataFrame
+from pandas import DataFrame, Series
 from scipy import stats
 
 from bocoel import Manager
 from bocoel.core.exams import columns
 
+LOGGER = structlog.get_logger()
 
-def default_non_inc_opt() -> list[str]:
-    return ["Kmeans({})", "KMedoids({})"]
+SPEARMAN_R = "spearmanr"
 
 
 def main(
     *,
     path: str = "./results",
-    ground_truth_optimizer: str = "BruteForce()",
-    non_incremental_optimizers: list[str] = default_non_inc_opt(),
+    ground_truth: str = "BruteForce()",
+    non_incremental: str = "Kmeans({}),KMedoids({})",
 ) -> None:
-    sns.set_theme()
-
     results = Manager.load(path)
-    ref_emb = reference_embeddings(results, ground_truth_optimizer)
 
+    # Get the unique storages and optimizers.
     storages = sorted(set(results[columns.STORAGE]))
-    models = sorted(set(results[columns.MODEL]))
     optimizers = sorted(set(results[columns.OPTIMIZER]))
+    indices = sorted(set(results[columns.INDEX]))
 
-    if len(storages) != len(ref_emb):
-        raise ValueError(
-            f"Expected {len(storages)} storages, got {len(ref_emb)} references."
-        )
+    # Non incremental and incremental optimizers.
+    # Here incremental optimizers means that steps are taken sequentially,
+    # whereas non incremental optimizers means that only the last step is used,
+    # and that multiple runs are joined together for analysis.
+    non_inc_opts = non_incremental.split(",")
+    inc_opts = set(optimizers).difference(non_inc_opts).difference([ground_truth])
 
-    # Filter only the latest runs.
-    latest_md5 = results.sort_values(columns.TIME, ascending=False).drop_duplicates(
-        [columns.MODEL, columns.STORAGE, columns.OPTIMIZER]
-    )[columns.MD5]
-    results = results[results[columns.MD5].isin(latest_md5)]
+    # Last steps, both in time and step index.
+    last = last_steps(results)
 
-    metrics = metric_by_storage_optimizer_steps(results, storages, optimizers, ref_emb)
+    ref_emb = reference_embeddings(last, ground_truth)
 
-    # Collect the optimizer where only the last step is used together into a single run.
-    metrics = collect_non_incremental(metrics, non_incremental_optimizers)
+    non_inc_last_steps = last_for_non_inc(last, non_inc_opts, optimizers)
+    inc_results = results[results[columns.OPTIMIZER].isin(inc_opts)]
 
-    # Plot results.
-    plotting_df = DataFrame(
-        [
-            {
-                "storage": storage,
-                "optimizer": optimizer,
-                "step": step,
-                "spearmanr": spearmanr,
-            }
-            for (storage, optimizer, step), spearmanr in metrics.items()
-            if optimizer != ground_truth_optimizer
-        ]
-    )
+    experiments = pd.concat([non_inc_last_steps, inc_results])
 
-    for store in storages:
-        data = plotting_df[plotting_df["storage"] == store]
-        sns.lineplot(data, x="step", y="spearmanr", hue="optimizer")
+    plotting_results = metrics(experiments, ref_emb)
+
+    for store, index in itertools.product(storages, indices):
+        storage_match = plotting_results[columns.STORAGE] == store
+        index_match = plotting_results[columns.INDEX] == index
+        data = plotting_results[storage_match & index_match]
+        data = data.sort_values(columns.STEP_IDX)
+
+        sns.lineplot(
+            data, x=columns.STEP_IDX, y=SPEARMAN_R, hue=columns.OPTIMIZER
+        ).set_title(f"{store} - {index}")
         plt.show()
 
 
-def collect_non_incremental(
-    metrics: dict[tuple[str, str, int], float], non_incremental_optimizers: list[str]
-) -> dict[tuple[str, str, int], float]:
+def last_steps(df: DataFrame) -> DataFrame:
+    cols = columns.STORAGE, columns.INDEX, columns.MODEL, columns.OPTIMIZER
+    return df.sort_values(
+        [columns.TIME, columns.STEP_IDX], ascending=False
+    ).drop_duplicates(cols)
+
+
+def metrics(
+    experiments: DataFrame, ref_emb: dict[tuple[str, str], NDArray]
+) -> DataFrame:
     """
-    Colllect the optimizers where only the last step makes sense (and is run multiple times).
-    """
-
-    # Mapping from the matched items to the tuple of count, template.
-    matched: dict[str, tuple[int, str]] = {}
-
-    for _, optimizer, step in metrics.keys():
-        for nio in non_incremental_optimizers:
-            # Skip if unmatched.
-            if parse.parse(nio, optimizer) is None:
-                continue
-
-            # Previously matched. Preverse the larger count.
-            # Skip if step isn't as large as the previously encountered.
-            if optimizer in matched:
-                prev_step, _ = matched[optimizer]
-
-                if step < prev_step:
-                    continue
-
-            # Matched.
-            matched[optimizer] = step, nio
-
-    results: dict[tuple[str, str, int], float] = {}
-    for (store, optimizer, step), value in metrics.items():
-        # Prevoiusly matched against the template.
-        if optimizer in matched:
-            matched_step, template = matched[optimizer]
-
-            # Only update if the step matches (largest one).
-            if step == matched_step:
-                results[store, template, step] = value
-        else:
-            results[store, optimizer, step] = value
-
-    return results
-
-
-def metric_by_storage_optimizer_steps(
-    results: DataFrame,
-    storages: list[str],
-    optimizers: list[str],
-    ref_emb: dict[str, NDArray],
-) -> dict[tuple[str, str, int], float]:
-    """
-    Calculate the Spearman's R metric for each storage, optimizer and step.
+    Calculate the metrics for the experiments.
 
     Parameters:
-        results: The results DataFrame.
-        storages: The list of storages.
-        optimizers: The list of optimizers.
+        experiments: The experiments DataFrame.
         ref_emb: The reference embeddings.
 
+
     Returns:
-        A dictionary with the Spearman's R metric for each storage, optimizer and step.
+        The metrics DataFrame.
     """
 
-    metrics: dict[tuple[str, str, int], float] = {}
+    num_models = len(set(experiments[columns.MODEL]))
 
-    for st, opt in itertools.product(storages, optimizers):
-        storage_match = results[columns.STORAGE] == st
-        optimizer_match = results[columns.OPTIMIZER] == opt
-        by_st_opt = results[storage_match & optimizer_match]
+    metrics_records = []
 
-        min_steps = (
-            by_st_opt.sort_values(columns.STEP_IDX, ascending=False)
-            .drop_duplicates(columns.MODEL)[columns.STEP_IDX]
-            .min()
+    cols = columns.STORAGE, columns.INDEX, columns.OPTIMIZER, columns.STEP_IDX
+
+    for _, (store, idx, opt, step) in experiments[list(cols)].iterrows():
+        store_match = experiments[columns.STORAGE] == store
+        idx_match = experiments[columns.INDEX] == idx
+        opt_match = experiments[columns.OPTIMIZER] == opt
+        step_match = experiments[columns.STEP_IDX] == step
+        relevant = experiments[store_match & idx_match & opt_match & step_match]
+
+        if len(relevant) != num_models:
+            LOGGER.debug(
+                "Insufficient models, discarded.",
+                rows=len(relevant),
+                num_models=num_models,
+                storage=store,
+                index=idx,
+                optimizer=opt,
+                step=step,
+            )
+
+            continue
+
+        relevant = relevant.sort_values(columns.MODEL)
+        spearmanr = stats.spearmanr(
+            np.array(relevant[columns.ACC_AVG]), ref_emb[store]
+        ).statistic
+
+        metrics_records.append(
+            {
+                columns.STORAGE: store,
+                columns.INDEX: idx,
+                columns.OPTIMIZER: opt,
+                columns.STEP_IDX: step,
+                SPEARMAN_R: spearmanr,
+            }
         )
 
-        by_st_opt = by_st_opt.sort_values(columns.MODEL)
-        for step in range(min_steps):
-            embeddings = np.array(
-                by_st_opt[by_st_opt[columns.STEP_IDX] == step][columns.ACC_AVG]
-            )
+    return DataFrame.from_records(metrics_records)
 
-            storage_ref = ref_emb[st]
-            if len(embeddings) != len(storage_ref):
-                raise ValueError(
-                    f"Expected {len(storage_ref)} embeddings, got {len(embeddings)}."
-                )
 
-            statistics = stats.spearmanr(embeddings, storage_ref).statistic
-            metrics[st, opt, step] = statistics
+def last_for_non_inc(
+    last_steps: DataFrame, non_inc_opts: list[str], optimizers: list[str]
+) -> DataFrame:
+    """
+    Get the last steps for non incremental optimizers.
 
-            print(
-                f"Storage: {st}, Optimizer: {opt}, Step: {step}, Spearman's R: {statistics}"
-            )
-    return metrics
+    Parameters:
+        last_steps: The last steps DataFrame.
+        non_inc_opts: The non incremental optimizers.
+        optimizers: The optimizers.
+
+    Returns:
+        The last steps for non incremental optimizers.
+    """
+
+    # Mapping of the parsing from non incremental optimizer
+    # to the optimizer and step index.
+    non_inc_idx: dict[str, tuple[str, int]] = {}
+    for target, optim in itertools.product(non_inc_opts, optimizers):
+        if (parsed := parse.parse(target, optim)) is not None:
+            idx = int(parsed[0])
+            non_inc_idx[optim] = target, idx
+
+    def transform(row: Series) -> Series:
+        series = row.copy()
+
+        optimizer = series[columns.OPTIMIZER]
+        assert optimizer in non_inc_idx, f"Optimizer {optimizer} not found."
+
+        target, idx = non_inc_idx[optimizer]
+
+        series[columns.OPTIMIZER] = target
+        series[columns.STEP_IDX] = idx
+
+        return series
+
+    non_inc_keys = list(non_inc_idx.keys())
+    non_inc_last_steps = last_steps[last_steps[columns.OPTIMIZER].isin(non_inc_keys)]
+
+    return non_inc_last_steps.apply(transform, axis=1)
 
 
 def reference_embeddings(
-    results: DataFrame, ground_truth_optimizer: str
-) -> dict[str, NDArray]:
+    last_steps: DataFrame, truth: str
+) -> dict[tuple[str, str], NDArray]:
+    """
+    Get the reference embeddings for each corpus (storage and index).
+
+    Parameters:
+        last_steps: The last steps DataFrame.
+        truth: The ground truth optimizer.
+
+    Returns:
+        The reference embeddings.
+    """
+
+    # Number of unique models and storages.
+    indices = sorted(set(last_steps[columns.INDEX]))
+    storages = sorted(set(last_steps[columns.STORAGE]))
+    num_models = len(set(last_steps[columns.MODEL]))
+
     # Get the ground truth.
-    ground_truth = results[results[columns.OPTIMIZER] == ground_truth_optimizer]
-    final_step = ground_truth.sort_values(
-        columns.STEP_IDX, ascending=False
-    ).drop_duplicates([columns.MODEL, columns.STORAGE])
+    ground_truth = last_steps[last_steps[columns.OPTIMIZER] == truth]
 
-    references = final_step.sort_values(
-        [columns.ADAPTOR, columns.STORAGE, columns.MODEL]
-    )
-
-    storages = sorted(set(references[columns.STORAGE]))
-    models = sorted(set(references[columns.MODEL]))
-
-    if len(references) != len(storages) * len(models):
+    if len(ground_truth) != len(storages) * num_models * len(indices):
         raise ValueError(
-            f"Expected {len(storages) * len(models)} references, got {len(references)}."
+            f"Expected {len(storages)} * {num_models} * {len(indices)} references, got {len(ground_truth)}."
         )
 
-    ref_emb_by_dataset: dict[str, NDArray] = {}
-    for dataset in storages:
-        current_dataset = references[columns.STORAGE] == dataset
-        acc_avg_by_dataset = np.array(references[current_dataset][columns.ACC_AVG])
-        ref_emb_by_dataset[dataset] = acc_avg_by_dataset
+    # References by storage name.
+    references: dict[tuple[str, str], NDArray] = {}
+    for dataset, index in itertools.product(storages, indices):
+        dataset_match = ground_truth[columns.STORAGE] == dataset
+        index_match = ground_truth[columns.INDEX] == index
 
-    return ref_emb_by_dataset
+        matched = ground_truth[dataset_match & index_match]
+        matched = matched.sort_values(columns.MODEL)
+
+        acc_avg = np.array(matched[columns.ACC_AVG])
+
+        if len(acc_avg) != num_models:
+            raise ValueError(
+                f"Expected {num_models} for each storage and index, "
+                f"got {len(acc_avg)}."
+            )
+
+        references[dataset] = acc_avg
+
+    return references
 
 
 if __name__ == "__main__":
+    sns.set_theme()
+
     fire.Fire(main)
